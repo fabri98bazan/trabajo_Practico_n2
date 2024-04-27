c53e79ca3e05b9b1b5716c4f4713845fd0ec557e
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-04-27 09:36:51 -0300

    Ejercicio 3 Finalizado

--- tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio3/main/Main.java
+++ tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio3/main/Main.java
@@ -1,10 +1,23 @@
 package ar.edu.unju.fi.ejercicio3.main;
 
-public class Main {
+import ar.edu.unju.fi.ejercicio3.constantes.Provincia;
 
+public class Main {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i;
+		Provincia[] prov=Provincia.values();
+		for(Provincia p: prov)
+		{
+			System.out.println("\n");
+			System.out.println("\nPROVINCIA: "+p);
+			System.out.println("Cantidad de habitantes: "+p.getCant_poblacion());
+			System.out.println("superficie: "+p.getSuperficie());
+			System.out.println("Densidad poblacional: "+p.calcularDensidadPoblacional());	
+			System.out.println("\n");
+			System.out.println();
+		}
 
 	}
-
+	
 }
