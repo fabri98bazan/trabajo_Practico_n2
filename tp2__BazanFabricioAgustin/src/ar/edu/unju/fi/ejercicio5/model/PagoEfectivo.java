e43a97e011347e81dbfb84e40c84ab14a7f846ad
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-04-29 19:01:47 -0300

    Actualizaxión del ejercicio 5

--- tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
+++ tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
@@ -1,5 +1,65 @@
 package ar.edu.unju.fi.ejercicio5.model;
 
-public class PagoEfectivo {
+import java.time.LocalDate;
+
+import ar.edu.unju.fi.ejercicio5.interfaces.Pago;
+
+public class PagoEfectivo implements Pago {
+	
+	private double monto_pagado;
+	private LocalDate fecha_pago;
+	
+	
+
+	public PagoEfectivo(double monto_pagado, LocalDate fecha_pago) {
+		this.monto_pagado = monto_pagado;
+		this.fecha_pago = fecha_pago;
+	}
+	
+	
+
+	public double getMonto_pagado() {
+		return monto_pagado;
+	}
+
+
+
+	public void setMonto_pagado(double monto_pagado) {
+		this.monto_pagado = monto_pagado;
+	}
+
+
+
+	public LocalDate getFecha_pago() {
+		return fecha_pago;
+	}
+
+
+
+	public void setFecha_pago(LocalDate fecha_pago) {
+		this.fecha_pago = fecha_pago;
+	}
+
+
+	
+
+	@Override
+	public String toString() {
+		return "PagoEfectivo [monto_pagado=" + monto_pagado + ", fecha_pago=" + fecha_pago + "]";
+	}
+
+
+
+	@Override
+	public void realizarPago(double monto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void imprimirRecibo() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio5/model/PagoTarjeta.java
+++ tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio5/model/PagoTarjeta.java
@@ -1,5 +1,77 @@
 package ar.edu.unju.fi.ejercicio5.model;
 
-public class PagoTarjeta {
+import java.time.LocalDate;
 
+import ar.edu.unju.fi.ejercicio5.interfaces.Pago;
+
+public class PagoTarjeta implements Pago {
+
+	private String nro_tarjeta;
+	private LocalDate fecha_pago;
+	private double monto;
+	
+	
+	
+	public PagoTarjeta(String nro_tarjeta, LocalDate fecha_pago, double monto) {
+		this.nro_tarjeta = nro_tarjeta;
+		this.fecha_pago = fecha_pago;
+		this.monto = monto;
+	}
+	
+	
+
+	public String getNro_tarjeta() {
+		return nro_tarjeta;
+	}
+
+
+
+	public void setNro_tarjeta(String nro_tarjeta) {
+		this.nro_tarjeta = nro_tarjeta;
+	}
+
+
+
+	public LocalDate getFecha_pago() {
+		return fecha_pago;
+	}
+
+
+
+	public void setFecha_pago(LocalDate fecha_pago) {
+		this.fecha_pago = fecha_pago;
+	}
+
+
+
+	public double getMonto() {
+		return monto;
+	}
+
+
+
+	public void setMonto(double monto) {
+		this.monto = monto;
+	}
+
+	
+
+
+	@Override
+	public String toString() {
+		return "PagoTarjeta [nro_tarjeta=" + nro_tarjeta + ", fecha_pago=" + fecha_pago + ", monto=" + monto + "]";
+	}
+
+
+
+	@Override
+	public void realizarPago(double monto) {
+		
+	}
+
+	@Override
+	public void imprimirRecibo() {
+		
+	}
+	
 }
