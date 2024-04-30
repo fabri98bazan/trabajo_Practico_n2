9667b1d28b37a8bf2f3d5857b2ea0568bd14f236
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-04-30 06:29:21 -0300

    Actualizacion del ejercicio 6

--- tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio6/main/Main.java
+++ tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -3,7 +3,7 @@ package ar.edu.unju.fi.ejercicio6.main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio6/model/FelinoSalvaje.java
+++ tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio6/model/FelinoSalvaje.java
@@ -4,4 +4,50 @@ public class FelinoSalvaje {
 	private String nombre;
 	private byte edad;
 	private Float peso;
+	
+	
+	public FelinoSalvaje(String nombre, byte edad, Float peso) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.peso = peso;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public byte getEdad() {
+		return edad;
+	}
+
+
+	public void setEdad(byte edad) {
+		this.edad = edad;
+	}
+
+
+	public Float getPeso() {
+		return peso;
+	}
+
+
+	public void setPeso(Float peso) {
+		this.peso = peso;
+	}
+
+
+	@Override
+	public String toString() {
+		return "FelinoSalvaje [nombre=" + nombre + ", edad=" + edad + ", peso=" + peso + "]";
+	}
+	
+	
+	
 }
