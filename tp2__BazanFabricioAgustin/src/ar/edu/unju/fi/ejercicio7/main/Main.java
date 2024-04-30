b1c6fbe00692fa5e8869329497b0e2e7ed2f1c97
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-04-30 07:39:08 -0300

    Actualizacion del ejercicio 7

--- tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio7/main/Main.java
+++ tp2__BazanFabricioAgustin/src/ar/edu/unju/fi/ejercicio7/main/Main.java
@@ -1,10 +1,134 @@
 package ar.edu.unju.fi.ejercicio7.main;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio7.model.Producto;
+import ar.edu.unju.fi.ejercicio7.model.Producto.categoria;
+import ar.edu.unju.fi.ejercicio7.model.Producto.origenFabricacion;
+
 public class Main {
+	
+	public static Scanner sc=new Scanner(System.in);
+	private static List<Producto> productos;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		productos= new ArrayList<>();
+		int opc=0;
+		do
+		{
+			mostrarMenu();
+			opc=manejarExcepcionIngresoNumEntero();
+			switch(opc)
+			{
+				case 1:
+					break;
+					
+				case 2:
+					break;
+					
+				case 3:
+					break;
+					
+				case 4:
+					break;
+					
+				case 5:
+					break;
+					
+				case 6:
+					break;
+					
+				case 7: System.out.println("Fin del Programa!!"); break;
+				
+				default: System.out.println("La opcion ingresada no es correcta");
+			}
+		}while(opc!=7);
 
+	}
+	
+	/**
+	 * Muestra el menu
+	 */
+	
+	public static void mostrarMenu()
+	{
+		System.out.println("\n1. Mostrar Productos");
+		System.out.println("2. Mostrar Productos faltantes");
+		System.out.println("3. Incrementar los precios de productos en un %20");
+		System.out.println("4. Mostrar Productos Electrohogar y disponibles");
+		System.out.println("5. Ordenar los productos de forma descendente");
+		System.out.println("6. Mostrar Productos con nombres en mayusculas");
+		System.out.println("7. Salir");
+		System.out.print("\nIngresar opcion: ");
+	}
+	
+	/**
+	 * Manejar excepcion de numeros  enteros
+	 * @return
+	 */
+	
+	public static int manejarExcepcionIngresoNumEntero() {
+		boolean band=false;
+		int n=0;
+		do
+		{
+			band=false;
+			try
+			{
+				n=sc.nextInt();
+				band=true;
+			}
+			catch(InputMismatchException ex)
+			{
+				System.out.println("Error al ingresar");
+				sc.next();
+				
+			}
+		}while(band==false);
+		return n;
+	}
+	
+	/**
+	 * Precarga de Productos
+	 */
+	
+	public static void precargarProductos()
+	{
+		origenFabricacion[] org=origenFabricacion.values();
+		categoria[] cat=categoria.values();
+		Producto p1=new Producto(126,"Televisor_HD", 65800.55, true, org[2], cat[2]);
+		productos.add(p1);
+		Producto p2=new Producto(116,"SmartPhone_Samsung", 165800.59, false, org[0], cat[0]);
+		productos.add(p2);
+		Producto p3=new Producto(180,"Procesador_i5", 11800.50, false, org[3], cat[1]);
+		productos.add(p3);
+		Producto p4=new Producto(176,"Refrigerador", 565800.09, false, org[1], cat[2]);
+		productos.add(p4);
+		Producto p5=new Producto(111,"Ventilador", 5800.17, true, org[0], cat[2]);
+		productos.add(p5);
+		Producto p6=new Producto(109,"Pistola_Clavos", 15000.15, true, org[1], cat[3]);
+		productos.add(p6);
+		Producto p7=new Producto(198,"Taladro", 44250.00, true, org[0], cat[3]);
+		productos.add(p7);
+		Producto p8=new Producto(127,"Aire_Acondicionado", 789800.48, true, org[3], cat[2]);
+		productos.add(p8);
+		Producto p9=new Producto(123,"Samsung_GalaxyA9", 195500.62, false, org[1], cat[0]);
+		productos.add(p9);
+		Producto p10=new Producto(137,"Mouse_Inhalambrico", 13500, true, org[2], cat[1]);
+		productos.add(p10);
+		Producto p11=new Producto(191,"Monitor_34''", 78950.25, false, org[0], cat[1]);
+		productos.add(p11);
+		Producto p12=new Producto(138,"Teclado_Inhalambrico", 35000.98, true, org[1], cat[1]);
+		productos.add(p12);
+		Producto p13=new Producto(135,"Parlante_Bluethoe", 22598.99, false, org[2], cat[2]);
+		productos.add(p13);
+		Producto p14=new Producto(117,"Caja_Herramientas", 58000.00, true, org[3], cat[3]);
+		productos.add(p14);
+		Producto p15=new Producto(199,"Modem", 45600.00, false, org[1], cat[0]);
+		productos.add(p15);
 	}
 
 }
